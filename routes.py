3902b13f0ac0616e42acfd23b089f77854404462
BlazerChlopiec <blazer8002@gmail.com>
2024-01-28 21:03:25 +0100

    Footer On Up Right

--- routes.py
+++ routes.py
@@ -12,7 +12,7 @@ def index():
 def submit():
     steamid = request.form['steamid_input']
     
-    fake = True
+    fake = False
 
     if fake:
         fake_data = GameData("bobas", 12312, 123132, 'https://img.gg.deals/52/c2/d98a5c9eac66dfb7eb44f998b5751e2f6390_307xr176.jpg', 'https://img.gg.deals/52/c2/d98a5c9eac66dfb7eb44f998b5751e2f6390_307xr176.jpg')
