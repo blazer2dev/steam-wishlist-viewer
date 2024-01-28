7c85e61d90259f735f7d2ad3b76c204eff725354
BlazerChlopiec <blazer8002@gmail.com>
2024-01-28 19:17:58 +0100

    Proper Background Gradient

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
-import requests
 from wishlistScraper import WishlistScraper
+from GameData import GameData
 
 bp = Blueprint('routes', __name__)
 
@@ -11,6 +11,12 @@ def index():
 @bp.route('/submit', methods=['POST'])
 def submit():
     steamid = request.form['steamid_input']
+
+    fake_data = GameData("bobas", 12312, 123132, 'https://img.gg.deals/52/c2/d98a5c9eac66dfb7eb44f998b5751e2f6390_307xr176.jpg', 'https://img.gg.deals/52/c2/d98a5c9eac66dfb7eb44f998b5751e2f6390_307xr176.jpg')
+    fake_datas = []
+    for i in range(100):
+        fake_datas.append(fake_data)
+    return render_template('index.html', game_datas=fake_datas)
 
     try:
         steamid = int(steamid)
